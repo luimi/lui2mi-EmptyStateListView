381a6b2a0fcf3fa3e9420167399ab97558b19d69
Luis Miguel Meza <luimi@live.com>
2018-02-26 19:18:24 -0500

    init

---
+++ demo/src/main/java/com/lui2mi/emptystatedemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.lui2mi.emptystatedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import com.lui2mi.emptystatelistview.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ListView list;
+    ArrayAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list=findViewById(R.id.es_prevew);
+        adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,
+                android.R.id.text1,new ArrayList());
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.show_top:
+                list.showTopProgress();
+                break;
+            case R.id.show_center:
+                list.showCenterProgress();
+                break;
+            case R.id.hide_top:
+                list.hideTopProgress();
+                break;
+            case R.id.hide_center:
+                list.hideCenterProgress();
+                break;
+            case R.id.add:
+                adapter.add("Item");
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.remove:
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+                break;
+
+        }
+    }
+}
